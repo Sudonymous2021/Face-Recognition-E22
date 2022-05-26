d55548da786e672ce863e6f99c81c6b2ade1aaa6
Sudonymous2021 <ss942001@student.nitw.ac.in>
2022-05-26 18:42:40 +0530

    commit

--- Frontend/src/components/NavbarComp.js
+++ Frontend/src/components/NavbarComp.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { Nav, Navbar, Container } from 'react-bootstrap';
 
 export default function NavbarComp() {
